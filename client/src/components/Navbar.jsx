351210889def70d46d593d910193af074f7e31a8
saokeems <saokeems@gmail.com>
2026-09-05 14:43:44 +0700

    feature/setup-core-client-server

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between py-2 gap-2">
+      <div>
+        <Link to="/">Logo</Link>
+      </div>
+      <div className="flex gap-4">
+        <Link to="/">Home</Link>
+        <Link to="/products">Products</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/custom-order">Custom Order</Link>
+      </div>
+      <div className="flex gap-4">
+        <Link to="/checkout">Cart</Link>
+        <Link to="/signin">Login/Register</Link>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
