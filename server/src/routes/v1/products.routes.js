aea63fb6f9be5028a2629d52418e33102b711f08
Chatchayapf <chatchaya.pf@gmai.com>
2026-09-17 22:09:00 +0700

    feature/create product-model

--- server/src/routes/v1/products.routes.js
+++ server/src/routes/v1/products.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { products } from "../../fakeDB/fakeProducts.js";
+// import { products } from "../../fakeDB/fakeProducts.js";
+import { Product } from "../../models/product.model.js";
 
 export const router = Router();
 
-router.get("/", (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
-    res.json(products);
+    const products = await Product.find()
+    return res.status(200).json(products);
   } catch (error) {
     next(error);
   }
