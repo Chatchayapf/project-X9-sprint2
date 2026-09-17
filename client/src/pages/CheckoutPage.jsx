77ba9c6c076f3a4eb899ce8b960ecf1bf889fc4b
PorpleumX <warakorn.3p@gmail.com>
2026-09-17 15:30:34 +0700

    all frontend not full

--- client/src/pages/CheckoutPage.jsx
+++ client/src/pages/CheckoutPage.jsx
@@ -1,6 +1,15 @@
+import Checkout from "../components/Checkout";
+import { useCart } from "../context/CartContext/CartContext";
+
 const CheckoutPage = () => {
+  const { cartItems, clearCart } = useCart();
+
   return (
-    <div>CheckoutPage</div>
-  )
-}
-export default CheckoutPage
+    <Checkout
+      cartItems={cartItems}
+      clearCart={clearCart}
+    />
+  );
+};
+
+export default CheckoutPage;
