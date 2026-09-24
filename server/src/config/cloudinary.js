a4a6b74d4776d02ef733cd1113c4c1510c855450
saokeems <saokeems@gmail.com>
2026-09-24 12:11:57 +0700

    feat(admin)/create validateObjectId upload middlewares and admin-controller-routes and test

--- server/src/config/cloudinary.js
+++ server/src/config/cloudinary.js
@@ -1,36 +1,52 @@
 import { v2 as cloudinary } from "cloudinary";
 
-const cloudinary = require("cloudinary").v2;
-
 cloudinary.config({
-    cloud_name: "my7ipdv4",
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export async function uploadImage(img) {
-    const res = await cloudinary.uploader.upload(img, {
-        folder: "digi9_product",
-    });
-    if (!res) {
-        throw new Error("Can't connect to Cloudinary!!");
-    }
-    const img_url = cloudinary.url(res.public_id, {
-        transformation: [
-            {
-                quality: "auto",
-                fetch_format: "auto",
-            },
-            {
-                width: 1200,
-                height: 1200,
-                crop: "fill",
-                gravity: "auto",
-            },
-        ],
-    });
+  const res = await cloudinary.uploader.upload(img, {
+    folder: "digi9_product",
+  });
+  if (!res) {
+    throw new Error("Can't connect to Cloudinary!!");
+  }
+  const img_url = cloudinary.url(res.public_id, {
+    transformation: [
+      {
+        quality: "auto",
+        fetch_format: "auto",
+      },
+      {
+        width: 1200,
+        height: 1200,
+        crop: "fill",
+        gravity: "auto",
+      },
+    ],
+  });
 
-    console.log(img_url);
+  console.log(img_url);
 
-    return img_url;
+  return img_url;
+}
+
+export const getPublicIdFromUrl = (url) => {
+  const parts = url.split("/");
+  const filename = parts.pop().split(".")[0];
+  const folder = parts.pop();
+  return `${folder}/${filename}`;
+};
+
+export async function deleteImage(url) {
+  try {
+    const publicId = getPublicIdFromUrl(url);
+    const res = await cloudinary.uploader.destroy(publicId);
+    return res;
+  } catch (error) {
+    console.error("Error deleting image from Cloudinary:", error);
+    throw error;
+  }
 }
---
+++ server/src/middlewares/validateObjectId.middleware.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const validateObjectId = (req, res, next) => {
+  const { id } = req.params;
+
+  if (!mongoose.isValidObjectId(id)) {
+    return res.status(400).json({ success: false, message: "Invalid product id" });
+  }
+
+  next();
+};
